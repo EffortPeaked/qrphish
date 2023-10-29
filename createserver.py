2813d9b92d0177f3c8f0351c9c95ad327b849799
EffortPeaked <149322073+EffortPeaked@users.noreply.github.com>
2023-10-29 16:26:21 -0700

    Create createserver.py

---
+++ createserver.py
@@ -0,0 +1,28 @@
+#in progress
+
+#starting code from https://github.com/xenjin450/qrcodephishing/blob/main/qrattack.py:
+
+import qrcode 
+import os 
+import subprocess 
+import requests 
+import webbrowser
+import qrcode
+import PIL 
+
+#Define QRCode
+qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+#Input QRCodeData 
+img = qr.add_data("http://malicioussite.com/")
+#Specify Image Data Type
+type = (img)
+#Creates a Image from the QRData
+img = qr.make_image(fill_color="black", back_color="white")
+#Save Image File
+img.save("QRAttack.jpg")
